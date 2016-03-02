0b4ae810da1b90d09f615e8904be622e38cb5d75
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2016-03-02 16:15:27 +0100

    stworzenie maven project do zadania 2

---
+++ Lab3/zad2/src/main/java/mavenTest/zad2/Psikus.java
@@ -0,0 +1,9 @@
+package mavenTest.zad2;
+
+public interface Psikus {
+	Integer CyfroKrad(Integer liczba);
+	Integer HultajChochla(Integer liczba);
+	Integer Nieksztaltek (Integer liczba);
+	Integer Heheszki(Integer liczba);
+	boolean Titit(Integer liczba_dziel);
+}
