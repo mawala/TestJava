765815a25bd3f12f46e3ae0e9b37ce9f48866db7
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2016-03-16 19:46:51 +0100

    dodanie EasyMock test

--- Lab5/src/test/java/com/example/mockdemo/app/MessageAppEasyMockTest.java
+++ Lab5/src/test/java/com/example/mockdemo/app/MessageAppEasyMockTest.java
@@ -3,13 +3,14 @@ package com.example.mockdemo.app;
 import static org.junit.Assert.*;
 import static org.easymock.EasyMock.*;
 
-import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import com.example.mockdemo.messenger.ConnectionStatus;
+import com.example.mockdemo.messenger.MalformedRecipientException;
 import com.example.mockdemo.messenger.MessageService;
+import com.example.mockdemo.messenger.SendingStatus;
 
 public class MessageAppEasyMockTest {
 
@@ -18,10 +19,10 @@ public class MessageAppEasyMockTest {
 	
 	private final String VALID_SERVER = "inf.ug.edu.pl";
 	private final String INVALID_SERVER = "inf.ug.edu.eu";
-	private final String INVALID_SERVER2 = "abc";
+	private final String INVALID_SERVER2 = "err";
 
-	private final String VALID_MESSAGE = "some message";
-	private final String INVALID_MESSAGE = "ab";
+	private final String VALID_MESSAGE = "correct message";
+	private final String INVALID_MESSAGE = "er";
 	
 	@Before
 	public void setUp() throws Exception {
@@ -36,12 +37,74 @@ public class MessageAppEasyMockTest {
 	}
 
 	@Test
-	public void testConnectionCorrect() {
+	public void testConnection_CorrectServer() {
 		expect(mockService.checkConnection(VALID_SERVER)).andReturn(ConnectionStatus.SUCCESS);
 		replay(mockService);
 		int result = messenger.testConnection(VALID_SERVER);
 		assertEquals(0, result);
 		verify(mockService);
 	}
+	
+	@Test
+	public void testConnection_CorrectServer_WithoutConnection() {
+		expect(mockService.checkConnection(VALID_SERVER)).andReturn(ConnectionStatus.FAILURE);
+		replay(mockService);
+		int result = messenger.testConnection(VALID_SERVER);
+		assertEquals(1, result);
+		verify(mockService);
+	}
 
+	@Test
+	public void testConnection_WrongServer() {
+		expect(mockService.checkConnection(INVALID_SERVER)).andReturn(ConnectionStatus.FAILURE);
+		replay(mockService);
+		int result = messenger.testConnection(INVALID_SERVER);
+		assertEquals(1, result);
+		verify(mockService);
+	}
+	
+	@Test
+	public void sendMessage_Correct() throws MalformedRecipientException {
+		expect(mockService.send(VALID_SERVER, VALID_MESSAGE)).andReturn(SendingStatus.SENT);
+		replay(mockService);
+		int result = messenger.sendMessage(VALID_SERVER, VALID_MESSAGE);
+		assertEquals(0, result);
+		verify(mockService);
+	}
+	
+	@Test
+	public void sendMessage_Correct_NotSent() throws MalformedRecipientException {
+		expect(mockService.send(VALID_SERVER, VALID_MESSAGE)).andReturn(SendingStatus.SENDING_ERROR);
+		replay(mockService);
+		int result = messenger.sendMessage(VALID_SERVER, VALID_MESSAGE);
+		assertEquals(1, result);
+		verify(mockService);
+	}
+	
+	@Test
+	public void sendMessage_WrongServer() throws MalformedRecipientException {
+		expect(mockService.send(INVALID_SERVER2, VALID_MESSAGE)).andThrow(new MalformedRecipientException());
+		replay(mockService);
+		int result = messenger.sendMessage(INVALID_SERVER2, VALID_MESSAGE);
+		assertEquals(2, result);
+		verify(mockService);
+	}
+	
+	@Test
+	public void sendMessage_WrongMessage() throws MalformedRecipientException {
+		expect(mockService.send(VALID_SERVER, INVALID_MESSAGE)).andThrow(new MalformedRecipientException());
+		replay(mockService);
+		int result = messenger.sendMessage(VALID_SERVER, INVALID_MESSAGE);
+		assertEquals(2, result);
+		verify(mockService);
+	}
+	
+	@Test
+	public void sendMessage_WrongServer_WrongMessage() throws MalformedRecipientException {
+		expect(mockService.send(INVALID_SERVER2, INVALID_MESSAGE)).andThrow(new MalformedRecipientException());
+		replay(mockService);
+		int result = messenger.sendMessage(INVALID_SERVER2, INVALID_MESSAGE);
+		assertEquals(2, result);
+		verify(mockService);
+	}
 }
