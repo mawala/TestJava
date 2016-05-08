fef9e6fd54a7996827acb43032c4f7ea9319a567
Marta Walczak <mwalczak@sigma.ug.edu.pl>
2016-05-08 21:50:17 +0200

    added test for other browser

---
+++ Lab11/src/test/java/mavenTest/Selenium/FirefoxTest.java
@@ -0,0 +1,51 @@
+package mavenTest.Selenium;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import mavenTest.Selenium.Helpers.Strings;
+import mavenTest.Selenium.Pages.HomePage;
+import mavenTest.Selenium.Pages.LoginPage;
+import mavenTest.Selenium.Pages.RepositoriesPage;
+
+public class FirefoxTest {
+
+	private WebDriver driver;
+	
+	@Before
+	public void setUp() {
+		driver = new FirefoxDriver();
+	}
+
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+
+	@Test
+	public void LoginCorrectTest() {
+		HomePage home = new HomePage(driver);
+		driver.get(home.getWebLink());
+		home.clickLoginButton();
+		
+		LoginPage login = new LoginPage(driver);
+		login.logIn(Strings.getEmail(), Strings.getPassword());
+		
+		(new WebDriverWait(driver, 10)).until(new ExpectedCondition<Boolean>() {
+            public Boolean apply(WebDriver d) {
+                return d.getTitle().toLowerCase().contains("home");
+            }
+        });
+		
+		RepositoriesPage repos = new RepositoriesPage(driver);
+		Assert.assertNotEquals(0, repos.numberRepos());
+	}
+}
